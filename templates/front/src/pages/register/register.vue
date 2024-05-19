260808c5787fe85f6cad671345d47eac55e59a10
ustddd <y3052909573@gmail.com>
2024-05-20 00:02:36 +0800

    front/src update

---
+++ templates/front/src/pages/register/register.vue
@@ -0,0 +1,334 @@
+<template>
+	<div>
+
+	<div class="container" :style='{"minHeight":"100vh","alignItems":"center","background":"url(http://codegen.caihongy.cn/20240305/c46698d0e9e14ad1b5729fb8a33a6d90.jpg)","display":"flex","width":"100%","backgroundSize":"cover","backgroundPosition":"center center","backgroundRepeat":"no-repeat","justifyContent":"center"}'>
+		<el-form class='rgs-form' v-if="pageFlag=='register'" :style='{"minHeight":"800px","padding":"20px 2vw 0 5vw","margin":"40px auto 40px","borderRadius":"0px","flexWrap":"wrap","background":"#fff","display":"flex","width":"40vw","position":"relative","height":"auto"}' ref="registerForm" :model="registerForm" :rules="rules">
+			<div v-if="false" :style='{"width":"100%","margin":"0 0 10px 0","lineHeight":"44px","fontSize":"20px","color":"rgba(64, 158, 255, 1)","textAlign":"center"}'>USER / REGISTER</div>
+			<div v-if="true" :style='{"margin":"20px auto 20px","color":"#2a8ec0","textAlign":"center","width":"90%","letterSpacing":"2px","lineHeight":"44px","fontSize":"32px","fontWeight":"600"}'>Web服务统一身份认证协议注册</p></div>
+			<el-form-item :style='{"width":"400px","padding":"0","margin":"0 auto 10px","height":"auto"}' v-if="tableName=='yonghu'" prop="yonghuzhanghao">
+				<div v-if="true" :style='{"padding":"0 10px","color":"#333","textAlign":"right","left":"-150px","background":"none","display":"inline-block","width":"150px","lineHeight":"36px","fontSize":"14px","position":"absolute"}' :class="changeRules('yonghuzhanghao')?'required':''">用户账号：</div>
+				<el-input v-model="registerForm.yonghuzhanghao"  placeholder="请输入用户账号" />
+			</el-form-item>
+			<el-form-item :style='{"width":"400px","padding":"0","margin":"0 auto 10px","height":"auto"}' v-if="tableName=='yonghu'" prop="yonghuxingming">
+				<div v-if="true" :style='{"padding":"0 10px","color":"#333","textAlign":"right","left":"-150px","background":"none","display":"inline-block","width":"150px","lineHeight":"36px","fontSize":"14px","position":"absolute"}' :class="changeRules('yonghuxingming')?'required':''">用户姓名：</div>
+				<el-input v-model="registerForm.yonghuxingming"  placeholder="请输入用户姓名" />
+			</el-form-item>
+			<el-form-item :style='{"width":"400px","padding":"0","margin":"0 auto 10px","height":"auto"}' v-if="tableName=='yonghu'" prop="mima">
+				<div v-if="true" :style='{"padding":"0 10px","color":"#333","textAlign":"right","left":"-150px","background":"none","display":"inline-block","width":"150px","lineHeight":"36px","fontSize":"14px","position":"absolute"}' :class="changeRules('mima')?'required':''">密码：</div>
+				<el-input v-model="registerForm.mima" type="password" placeholder="请输入密码" />
+			</el-form-item>
+			<el-form-item :style='{"width":"400px","padding":"0","margin":"0 auto 10px","height":"auto"}' v-if="tableName=='yonghu'" prop="mima2">
+				<div v-if="true" :style='{"padding":"0 10px","color":"#333","textAlign":"right","left":"-150px","background":"none","display":"inline-block","width":"150px","lineHeight":"36px","fontSize":"14px","position":"absolute"}' :class="changeRules('mima')?'required':''">确认密码：</div>
+				<el-input v-model="registerForm.mima2" type="password" placeholder="请再次输入密码" />
+			</el-form-item>
+			<el-form-item :style='{"width":"400px","padding":"0","margin":"0 auto 10px","height":"auto"}' v-if="tableName=='yonghu'" prop="xingbie">
+				<div v-if="true" :style='{"padding":"0 10px","color":"#333","textAlign":"right","left":"-150px","background":"none","display":"inline-block","width":"150px","lineHeight":"36px","fontSize":"14px","position":"absolute"}' :class="changeRules('xingbie')?'required':''">性别：</div>
+                <el-select v-model="registerForm.xingbie" placeholder="请选择性别" >
+                  <el-option
+                      v-for="(item,index) in yonghuxingbieOptions"
+                      :key="index"
+                      :label="item"
+                      :value="item">
+                  </el-option>
+                </el-select>
+			</el-form-item>
+			<el-form-item :style='{"width":"400px","padding":"0","margin":"0 auto 10px","height":"auto"}' v-if="tableName=='yonghu'" prop="touxiang">
+				<div v-if="true" :style='{"padding":"0 10px","color":"#333","textAlign":"right","left":"-150px","background":"none","display":"inline-block","width":"150px","lineHeight":"36px","fontSize":"14px","position":"absolute"}' :class="changeRules('touxiang')?'required':''">头像：</div>
+                <file-upload
+					tip="点击上传头像"
+					action="file/upload"
+					:limit="1"
+					:multiple="true"
+					:fileUrls="registerForm.touxiang?registerForm.touxiang:''"
+					@change="yonghutouxiangUploadChange"
+				></file-upload>
+			</el-form-item>
+			<el-form-item :style='{"width":"400px","padding":"0","margin":"0 auto 10px","height":"auto"}' v-if="tableName=='yonghu'" prop="dianhuahaoma">
+				<div v-if="true" :style='{"padding":"0 10px","color":"#333","textAlign":"right","left":"-150px","background":"none","display":"inline-block","width":"150px","lineHeight":"36px","fontSize":"14px","position":"absolute"}' :class="changeRules('dianhuahaoma')?'required':''">电话号码：</div>
+				<el-input v-model="registerForm.dianhuahaoma"  placeholder="请输入电话号码" />
+			</el-form-item>
+			<el-button :style='{"border":"0","cursor":"pointer","padding":"0 24px","margin":"0 auto","color":"#fff","display":"block","outline":"none","borderRadius":"4px","background":"#3795c4","width":"400px","fontSize":"16px","height":"50px","order":"30"}' type="primary" @click="submitForm('registerForm')">注册</el-button>
+			<el-button :style='{"border":"0","cursor":"pointer","padding":"0 0px","margin":"0px auto","color":"#999","textAlign":"right","display":"none","outline":"none","borderRadius":"4px","background":"none","width":"400px","fontSize":"14px","height":"44px","order":"40"}' @click="resetForm('registerForm')">重置</el-button>
+			<router-link :style='{"cursor":"pointer","padding":"0 0px","margin":"12px auto 20px","color":"#3795c4","textAlign":"center","display":"block","width":"400px","lineHeight":"1","fontSize":"14px","textDecoration":"none","order":"50"}' to="/login">已有账户登录</router-link>
+			<div class="idea1" :style='{"width":"100%","background":"red","display":"none","height":"40px"}'></div>
+			<div class="idea2" :style='{"width":"100%","background":"blue","display":"none","height":"40px"}'></div>
+		</el-form>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+    //数据集合
+    data() {
+		return {
+            pageFlag : '',
+			tableName: '',
+			registerForm: {},
+			forgetForm: {},
+			rules: {},
+			requiredRules: {},
+            yonghuxingbieOptions: [],
+		}
+    },
+	mounted() {
+		if(this.$route.query.pageFlag=='register'){
+			this.tableName = this.$route.query.role;
+			if(this.tableName=='yonghu'){
+				this.registerForm = {
+					yonghuzhanghao: '',
+					yonghuxingming: '',
+					mima: '',
+					mima2: '',
+					xingbie: '',
+					touxiang: '',
+					dianhuahaoma: '',
+				}
+			}
+			if ('yonghu' == this.tableName) {
+				this.requiredRules.yonghuzhanghao = [{ required: true, message: '请输入用户账号', trigger: 'blur' }]
+			}
+			if ('yonghu' == this.tableName) {
+				this.requiredRules.yonghuxingming = [{ required: true, message: '请输入用户姓名', trigger: 'blur' }]
+			}
+			if ('yonghu' == this.tableName) {
+				this.requiredRules.mima = [{ required: true, message: '请输入密码', trigger: 'blur' }]
+			}
+		}
+	},
+    created() {
+		this.pageFlag = this.$route.query.pageFlag;
+		if(this.$route.query.pageFlag=='register'){
+		  if ('yonghu' == this.tableName) {
+			this.rules.yonghuzhanghao = [{ required: true, message: '请输入用户账号', trigger: 'blur' }];
+		  }
+		  if ('yonghu' == this.tableName) {
+			this.rules.yonghuxingming = [{ required: true, message: '请输入用户姓名', trigger: 'blur' }];
+		  }
+		  if ('yonghu' == this.tableName) {
+			this.rules.mima = [{ required: true, message: '请输入密码', trigger: 'blur' }];
+		  }
+			this.yonghuxingbieOptions = "男,女".split(',');
+		  if ('yonghu' == this.tableName) {
+			this.rules.dianhuahaoma = [{ required: true, validator: this.$validate.isMobile, trigger: 'blur' }];
+		  }
+		}
+    },
+    //方法集合
+    methods: {
+		changeRules(name){
+			if(this.requiredRules[name]){
+				return true
+			}
+			return false
+		},
+      // 获取uuid
+      getUUID () {
+        return new Date().getTime();
+      },
+        // 下二随
+      yonghutouxiangUploadChange(fileUrls) {
+          this.registerForm.touxiang = fileUrls.replace(new RegExp(this.$config.baseUrl,"g"),"");
+      },
+
+        // 多级联动参数
+
+
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            var url=this.tableName+"/register";
+				if((!this.registerForm.yonghuzhanghao) && `yonghu` == this.tableName){
+					this.$message.error(`用户账号不能为空`);
+					return
+				}
+				if((!this.registerForm.yonghuxingming) && `yonghu` == this.tableName){
+					this.$message.error(`用户姓名不能为空`);
+					return
+				}
+               if(`yonghu` == this.tableName && this.registerForm.mima!=this.registerForm.mima2) {
+                this.$message.error(`两次密码输入不一致`);
+                return
+               }
+				if((!this.registerForm.mima) && `yonghu` == this.tableName){
+					this.$message.error(`密码不能为空`);
+					return
+				}
+					if(`yonghu` == this.tableName && this.registerForm.dianhuahaoma &&(!this.$validate.isMobile2(this.registerForm.dianhuahaoma))){
+						this.$message.error(`电话号码应输入手机格式`);
+						return
+					}
+            this.$http.post(url, this.registerForm).then(res => {
+              if (res.data.code === 0) {
+                this.$message({
+                  message: '注册成功',
+                  type: 'success',
+                  duration: 1500,
+                  onClose: () => {
+                    this.$router.push('/login');
+                  }
+                });
+              } else {
+                this.$message.error(res.data.msg);
+              }
+            });
+          } else {
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    }
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+	.container {
+		position: relative;
+		background: url(http://codegen.caihongy.cn/20240305/c46698d0e9e14ad1b5729fb8a33a6d90.jpg);
+		
+		.el-input {
+		  width: auto;
+		}
+		
+		.el-date-editor.el-input {
+			width: auto;
+		}
+		
+		.el-form-item /deep/ .el-form-item__content {
+						display: flex;
+						flex-wrap: wrap;
+					}
+		
+		.rgs-form .el-input /deep/ .el-input__inner {
+						border-radius: 8px;
+						padding: 0 10px;
+						outline: none;
+						color: #666;
+						background: #fff;
+						width: 400px;
+						font-size: 14px;
+						border-color: #c6c6c6;
+						border-width: 2px;
+						border-style: solid;
+						height: 50px;
+					}
+		
+		.rgs-form .el-select /deep/ .el-input__inner {
+						border-radius: 8px;
+						padding: 0 10px;
+						outline: none;
+						color: #666;
+						background: #fff;
+						width: 400px;
+						font-size: 14px;
+						border-color: #c6c6c6;
+						border-width: 2px;
+						border-style: solid;
+						min-width: 300px;
+						height: 50px;
+					}
+		
+		.rgs-form .el-date-editor /deep/ .el-input__inner {
+						border-radius: 8px;
+						padding: 0 10px 0 30px;
+						outline: none;
+						color: #666;
+						background: #fff;
+						width: 400px;
+						font-size: 14px;
+						border-color: #c6c6c6;
+						border-width: 2px;
+						border-style: solid;
+						min-width: 300px;
+						height: 50px;
+					}
+		
+		.rgs-form .el-date-editor /deep/ .el-input__inner {
+						border-radius: 8px;
+						padding: 0 10px 0 30px;
+						outline: none;
+						color: #666;
+						background: #fff;
+						width: 400px;
+						font-size: 14px;
+						border-color: #c6c6c6;
+						border-width: 2px;
+						border-style: solid;
+						min-width: 300px;
+						height: 50px;
+					}
+		
+		.rgs-form /deep/ .el-upload--picture-card {
+			background: transparent;
+			border: 0;
+			border-radius: 0;
+			width: auto;
+			height: auto;
+			line-height: initial;
+			vertical-align: middle;
+		}
+		
+		.rgs-form /deep/ .upload .upload-img {
+		  		  cursor: pointer;
+		  		  color: #c6c6c6;
+		  		  font-weight: 600;
+		  		  font-size: 24px;
+		  		  border-color: #c6c6c6;
+		  		  line-height: 80px;
+		  		  border-radius: 4px;
+		  		  background: #fff;
+		  		  width: 80px;
+		  		  border-width: 2px;
+		  		  border-style: solid;
+		  		  text-align: center;
+		  		  height: 80px;
+		  		}
+		
+		.rgs-form /deep/ .el-upload-list .el-upload-list__item {
+		  		  cursor: pointer;
+		  		  color: #c6c6c6;
+		  		  font-weight: 600;
+		  		  font-size: 24px;
+		  		  border-color: #c6c6c6;
+		  		  line-height: 80px;
+		  		  border-radius: 4px;
+		  		  background: #fff;
+		  		  width: 80px;
+		  		  border-width: 2px;
+		  		  border-style: solid;
+		  		  text-align: center;
+		  		  height: 80px;
+		  		}
+		
+		.rgs-form /deep/ .el-upload .el-icon-plus {
+		  		  cursor: pointer;
+		  		  color: #c6c6c6;
+		  		  font-weight: 600;
+		  		  font-size: 24px;
+		  		  border-color: #c6c6c6;
+		  		  line-height: 80px;
+		  		  border-radius: 4px;
+		  		  background: #fff;
+		  		  width: 80px;
+		  		  border-width: 2px;
+		  		  border-style: solid;
+		  		  text-align: center;
+		  		  height: 80px;
+		  		}
+	}
+	.required {
+		position: relative;
+	}
+	.required::after{
+				color: red;
+				left: 140px;
+				position: absolute;
+				content: "*";
+				order: 2;
+			}
+</style>
---
+++ templates/front/src/router/router.js
@@ -0,0 +1,181 @@
+import VueRouter from 'vue-router'
+
+//引入组件
+import Index from '../pages'
+import Home from '../pages/home/home'
+import Login from '../pages/login/login'
+import Register from '../pages/register/register'
+import Center from '../pages/center/center'
+import Messages from '../pages/messages/list'
+import Forum from '../pages/forum/list'
+import ForumAdd from '../pages/forum/add'
+import ForumDetail from '../pages/forum/detail'
+import MyForumList from '../pages/forum/myForumList'
+import Storeup from '../pages/storeup/list'
+import News from '../pages/news/news-list'
+import NewsDetail from '../pages/news/news-detail'
+import payList from '../pages/pay'
+
+import yonghuList from '../pages/yonghu/list'
+import yonghuDetail from '../pages/yonghu/detail'
+import yonghuAdd from '../pages/yonghu/add'
+import kaoqinqiandaoList from '../pages/kaoqinqiandao/list'
+import kaoqinqiandaoDetail from '../pages/kaoqinqiandao/detail'
+import kaoqinqiandaoAdd from '../pages/kaoqinqiandao/add'
+import qiandaojiluList from '../pages/qiandaojilu/list'
+import qiandaojiluDetail from '../pages/qiandaojilu/detail'
+import qiandaojiluAdd from '../pages/qiandaojilu/add'
+import newstypeList from '../pages/newstype/list'
+import newstypeDetail from '../pages/newstype/detail'
+import newstypeAdd from '../pages/newstype/add'
+import aboutusList from '../pages/aboutus/list'
+import aboutusDetail from '../pages/aboutus/detail'
+import aboutusAdd from '../pages/aboutus/add'
+import systemintroList from '../pages/systemintro/list'
+import systemintroDetail from '../pages/systemintro/detail'
+import systemintroAdd from '../pages/systemintro/add'
+
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+	return originalPush.call(this, location).catch(err => err)
+}
+
+//配置路由
+export default new VueRouter({
+	routes:[
+		{
+      path: '/',
+      redirect: '/index/home'
+    },
+		{
+			path: '/index',
+			component: Index,
+			children:[
+				{
+					path: 'home',
+					component: Home
+				},
+				{
+					path: 'center',
+					component: Center,
+				},
+				{
+					path: 'pay',
+					component: payList,
+				},
+				{
+					path: 'messages',
+					component: Messages
+				},
+				{
+					path: 'forum',
+					component: Forum
+				},
+				{
+					path: 'forumAdd',
+					component: ForumAdd
+				},
+				{
+					path: 'forumDetail',
+					component: ForumDetail
+				},
+				{
+					path: 'myForumList',
+					component: MyForumList
+				},
+				{
+					path: 'storeup',
+					component: Storeup
+				},
+				{
+					path: 'news',
+					component: News
+				},
+				{
+					path: 'newsDetail',
+					component: NewsDetail
+				},
+				{
+					path: 'yonghu',
+					component: yonghuList
+				},
+				{
+					path: 'yonghuDetail',
+					component: yonghuDetail
+				},
+				{
+					path: 'yonghuAdd',
+					component: yonghuAdd
+				},
+				{
+					path: 'kaoqinqiandao',
+					component: kaoqinqiandaoList
+				},
+				{
+					path: 'kaoqinqiandaoDetail',
+					component: kaoqinqiandaoDetail
+				},
+				{
+					path: 'kaoqinqiandaoAdd',
+					component: kaoqinqiandaoAdd
+				},
+				{
+					path: 'qiandaojilu',
+					component: qiandaojiluList
+				},
+				{
+					path: 'qiandaojiluDetail',
+					component: qiandaojiluDetail
+				},
+				{
+					path: 'qiandaojiluAdd',
+					component: qiandaojiluAdd
+				},
+				{
+					path: 'newstype',
+					component: newstypeList
+				},
+				{
+					path: 'newstypeDetail',
+					component: newstypeDetail
+				},
+				{
+					path: 'newstypeAdd',
+					component: newstypeAdd
+				},
+				{
+					path: 'aboutus',
+					component: aboutusList
+				},
+				{
+					path: 'aboutusDetail',
+					component: aboutusDetail
+				},
+				{
+					path: 'aboutusAdd',
+					component: aboutusAdd
+				},
+				{
+					path: 'systemintro',
+					component: systemintroList
+				},
+				{
+					path: 'systemintroDetail',
+					component: systemintroDetail
+				},
+				{
+					path: 'systemintroAdd',
+					component: systemintroAdd
+				},
+			]
+		},
+		{
+			path: '/login',
+			component: Login
+		},
+		{
+			path: '/register',
+			component: Register
+		},
+	]
+})
